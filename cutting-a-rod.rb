45d58cfe4be6635984ae69a1a690ade384d4e0b3
traveltriangle <adi.anu.ducs@gmail.com>
2015-02-15 22:00:44 +0530

    longest palindromic subsequence + cutting a rod

---
+++ cutting-a-rod.rb
@@ -0,0 +1,27 @@
+class CuttingARod
+	def initialize(len, val)
+		@len = len
+		@val = val
+		@dp_arr = Array.new(len.size) 
+	end
+	
+	def max_profit
+		i = 0
+		j = 0
+		@val.each do |l|
+			temp = [l]
+			j = i-1
+			while j >=0 
+				temp << @dp_arr[j] + @dp_arr[i-j-1] 
+				j -= 1
+			end
+			@dp_arr[i] = temp.max
+			i += 1
+		end
+		return @dp_arr.last
+	end
+	
+end
+obj = CuttingARod.new([1,2,3,4,5,6,7,8], [1, 5, 8, 9, 10, 17, 17, 20])
+op = obj.max_profit
+print ">>> op >> #{op}\n"
